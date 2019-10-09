9d6f5372c0115449889c27de506f0d7e7e734871
kaleb22 <kaleb22@hotmail.com>
2019-10-09 20:22:43 -0300

    project skeleton setup

---
+++ app/app.js
@@ -0,0 +1,7 @@
+// Declare app level module which depends on views, and core components
+let myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', function($scope) {
+
+  $scope.name = "Kaleb"; 
+});
